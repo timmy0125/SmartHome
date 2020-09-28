7a4cc6f07666b5f01ed804ff24ff6b34068b1254
Timmy <3981410timmy@gamil.com>
2020-09-28 16:17:42 +0800

    開門斷電

--- arduinoSmartHome/arduinoSmartHome.ino
+++ arduinoSmartHome/arduinoSmartHome.ino
@@ -2,16 +2,22 @@ void setup() {
  pinMode(12, OUTPUT);
   pinMode(11, OUTPUT);
   pinMode(7, OUTPUT);
-
+   pinMode(2, INPUT);
+Serial.begin(9600);
 }
 
-void loop() {
- digitalWrite(12 ,HIGH);
- digitalWrite(11 ,LOW);
- tone(7, 1000, 500);
- delay(500);
- digitalWrite(12 ,LOW);
- digitalWrite(11, HIGH);
- tone(7, 800, 500);
- delay(500);
+void loop() 
+{ 
+if(digitalRead(2)==HIGH) {
+  digitalWrite(12, HIGH);
+  digitalWrite(11, LOW);
+  tone(7, 1000, 500); 
+delay(500); 
+digitalWrite(12, LOW); 
+digitalWrite(11, HIGH); 
+tone(7, 800, 500); 
+delay(500); 
+} 
+else { 
+digitalWrite(12, LOW); digitalWrite(11, LOW); } 
 }

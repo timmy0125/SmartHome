59796bbb5b24ea36957c41d09bcbe22e9fa03f0e
Timmy <3981410timmy@gamil.com>
2020-09-28 16:37:20 +0800

    問題回應

--- arduinoSmartHome/arduinoSmartHome.ino
+++ arduinoSmartHome/arduinoSmartHome.ino
@@ -1,23 +1,29 @@
-void setup() {
- pinMode(12, OUTPUT);
-  pinMode(11, OUTPUT);
-  pinMode(7, OUTPUT);
-   pinMode(2, INPUT);
+void setup()
+{
+pinMode(12, OUTPUT);
+pinMode(11, OUTPUT);
+pinMode(7, OUTPUT);
+pinMode(2, INPUT);
 Serial.begin(9600);
 }
 
-void loop() 
-{ 
+void loop()
+{
 if(digitalRead(2)==HIGH) {
-  digitalWrite(12, HIGH);
-  digitalWrite(11, LOW);
-  tone(7, 1000, 500); 
-delay(500); 
-digitalWrite(12, LOW); 
-digitalWrite(11, HIGH); 
-tone(7, 800, 500); 
-delay(500); 
-} 
-else { 
-digitalWrite(12, LOW); digitalWrite(11, LOW); } 
+Serial.println("alert");
+digitalWrite(12, HIGH);
+digitalWrite(11, LOW);
+tone(7, 1000, 500);
+delay(500);
+digitalWrite(12, LOW);
+digitalWrite(11, HIGH);
+tone(7, 800, 500);
+delay(500);
+}
+else {
+Serial.println("normal");
+digitalWrite(12, LOW);
+digitalWrite(11, LOW);
+delay(1000);
+}
 }
